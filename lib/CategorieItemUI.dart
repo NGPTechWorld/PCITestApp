9c1bc908e7f8d2475f8c060defcad702077259cd
NGPTechWorld <ngpsupp@gmail.com>
2024-05-17 19:04:05 +0300

    add items to categori UI

--- lib/CategorieItemUI.dart
+++ lib/CategorieItemUI.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:hexcolor/hexcolor.dart';
 import 'package:pcitest/Explore.dart';
 import 'package:pcitest/Moduls.dart';
 
@@ -11,7 +13,122 @@ class CategorieItemUI extends StatelessWidget {
     return  Scaffold(
       appBar: AppBar(
         title: Center(child: Text(categorie.name)),
+        actions: [
+          Container(
+            padding: EdgeInsets.only(right: 15),
+            alignment: AlignmentDirectional.centerEnd,
+            child: MaterialButton(
+              shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(16)),
+              color: HexColor("#53B175"),
+              onPressed: (){
+                print("d");
+              },
+              minWidth: 30,
+              height: 40,
+              child: SvgPicture.asset("src/plus.svg",width: 15,),
+            ),
+          )
+        ],
+      ),
+      body:  Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+            child: GridView.count(
+              crossAxisCount:2,
+              childAspectRatio: 0.75,
+              children:categorie.items.map((item) => InkWell(
+                onTap: (){
+                  print(item);
+
+                },
+                child: Container(
+                    margin:EdgeInsets.all(10) ,
+                    decoration: BoxDecoration(
+                      border:Border.all(color:HexColor("#E2E2E2")),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Image.asset(item.img,height: 100,width: 120,fit: BoxFit.contain,),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(left: 10,top: 5),
+                          alignment: AlignmentDirectional.centerStart,
+                          child: FittedBox(
+                            fit: BoxFit.contain,
+                            child: Text(
+                              item.name,
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(left: 10,top: 5),
+                          alignment: AlignmentDirectional.centerStart,
+                          child: FittedBox(
+                            fit: BoxFit.contain,
+                            child: Text(
+                              item.descrption,
+                              style: TextStyle(
+                                color: HexColor("#7C7C7C"),
+                                fontSize: 16,
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 5,right: 10),
+                          child: Row(
+
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                padding: EdgeInsets.only(left: 10,top: 15),
+
+                                child: FittedBox(
+                                  fit: BoxFit.contain,
+                                  child: Text(
+                                    priceItem(item.price),
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18,
+                                    ),
+                                    textAlign: TextAlign.left,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                alignment: AlignmentDirectional.centerEnd,
+                                child: MaterialButton(
+                                  shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(16)),
+                                  color: HexColor("#53B175"),
+                                  onPressed: (){
+                                    print("d");
+                                  },
+                                  minWidth: 30,
+                                  height: 40,
+                                  child: SvgPicture.asset("src/plus.svg",width: 15,),
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    )
+                ),
+              )).toList(),
+            )
+        ),
       ),
     );
+  }
+  String priceItem(double p){
+    return "\$"+p.toString();
   }
 }
--- lib/Moduls.dart
+++ lib/Moduls.dart
@@ -2,25 +2,34 @@ import 'dart:ui';
 
 import 'package:hexcolor/hexcolor.dart';
 
+List<Item>CartItems=[];
+List<Item>Beverages=[
+  Item.name("Diet Coke", "355ml, Price", "src/Beverages/img1.png",1.99 ),
+  Item.name("Sprite Can", "355ml, Price", "src/Beverages/img2.png", 1.50),
+  Item.name("Apple & Grape Juice", "2L, Price", "src/Beverages/img3.png",5.99 ),
+  Item.name("Orenge Juice", "2L, Price", "src/Beverages/img4.png", 8.99),
+  Item.name("Coca Cola Can", "325ml, Price", "src/Beverages/img5.png", 4.99),
+  Item.name("Pepsi Can", "325ml, Price", "src/Beverages/img6.png", 4.99),
+];
 List<ItemCategoris>categorie=[
-  ItemCategoris.name("Frash Fruits & Vegetable", "src/c1.png", HexColor("#53B175")),
-  ItemCategoris.name("Cooking Oil & Ghee", "src/c2.png", HexColor("#F8A44C")),
-  ItemCategoris.name("Meat & Fish", "src/c3.png", HexColor("#F7A593")),
-  ItemCategoris.name("Bakery & Snacks", "src/c4.png", HexColor("#D3B0E0")),
-  ItemCategoris.name("Dairy & Eggs", "src/c5.png", HexColor("#FDE598")),
-  ItemCategoris.name("Beverages", "src/c6.png", HexColor("#B7DFF5")),
-  ItemCategoris.name("Frash Fruits & Vegetable", "src/c1.png", HexColor("#53B175")),
-  ItemCategoris.name("Cooking Oil & Ghee", "src/c2.png", HexColor("#F8A44C")),
-  ItemCategoris.name("Meat & Fish", "src/c3.png", HexColor("#F7A593")),
-  ItemCategoris.name("Bakery & Snacks", "src/c4.png", HexColor("#D3B0E0")),
-  ItemCategoris.name("Dairy & Eggs", "src/c5.png", HexColor("#FDE598")),
-  ItemCategoris.name("Beverages", "src/c6.png", HexColor("#B7DFF5")),
+  ItemCategoris.name("Frash Fruits & Vegetable", "src/c1.png", HexColor("#53B175"),[]),
+  ItemCategoris.name("Cooking Oil & Ghee", "src/c2.png", HexColor("#F8A44C"),[]),
+  ItemCategoris.name("Meat & Fish", "src/c3.png", HexColor("#F7A593"),[]),
+  ItemCategoris.name("Bakery & Snacks", "src/c4.png", HexColor("#D3B0E0"),[]),
+  ItemCategoris.name("Dairy & Eggs", "src/c5.png", HexColor("#FDE598"),[]),
+  ItemCategoris.name("Beverages", "src/c6.png", HexColor("#B7DFF5"),Beverages),
+  ItemCategoris.name("Frash Fruits & Vegetable", "src/c1.png", HexColor("#53B175"),[]),
+  ItemCategoris.name("Cooking Oil & Ghee", "src/c2.png", HexColor("#F8A44C"),[]),
+  ItemCategoris.name("Meat & Fish", "src/c3.png", HexColor("#F7A593"),[]),
+  ItemCategoris.name("Bakery & Snacks", "src/c4.png", HexColor("#D3B0E0"),[]),
+  ItemCategoris.name("Dairy & Eggs", "src/c5.png", HexColor("#FDE598"),[]),
+  ItemCategoris.name("Beverages", "src/c6.png", HexColor("#B7DFF5"),[]),
 ];
 class ItemCategoris{
   String name,img;
   Color backgroundColor;
-  List<Item>? items;
-  ItemCategoris.name(this.name, this.img, this.backgroundColor);
+  List<Item> items=[];
+  ItemCategoris.name(this.name, this.img, this.backgroundColor,this.items);
 }
 class Item{
   String name,descrption,img;
